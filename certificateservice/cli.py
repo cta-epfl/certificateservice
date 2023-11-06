73b49e4cdaea3b006b18ee5ec20b44604550b6cc
Bastien <bastien.wermeille@gmail.com>
2023-11-06 14:34:42 +0100

    Change port number

--- certificateservice/cli.py
+++ certificateservice/cli.py
@@ -5,7 +5,7 @@ from certificateservice.app import app
 
 def main():
     logging.basicConfig(level=logging.DEBUG)
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5001)
 
 
 if __name__ == "__main__":
