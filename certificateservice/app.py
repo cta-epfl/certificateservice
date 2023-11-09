373152c8f30a9f656e642df3c15498125cd634c0
Bastien <bastien.wermeille@gmail.com>
2023-11-09 10:43:50 +0100

    Format code

--- certificateservice/app.py
+++ certificateservice/app.py
@@ -1,6 +1,8 @@
 from datetime import date, datetime, timedelta
 from certificateservice.certificate import (
-    CertificateError, certificate_validity, verify_certificate
+    CertificateError,
+    certificate_validity,
+    verify_certificate,
 )
 from functools import wraps
 import os
@@ -13,8 +15,13 @@ import secrets
 import xml.etree.ElementTree as ET
 import importlib.metadata
 from flask import (
-    Blueprint, Flask, make_response, redirect, request,
-    session, render_template
+    Blueprint,
+    Flask,
+    make_response,
+    redirect,
+    request,
+    session,
+    render_template,
 )
 from flask_cors import CORS
 
@@ -24,18 +31,17 @@ import sentry_sdk
 from sentry_sdk.integrations.flask import FlaskIntegration
 
 sentry_sdk.init(
-    dsn='https://452458c2a6630292629364221bff0dee@o4505709665976320' +
-        '.ingest.sentry.io/4505709666762752',
+    dsn='https://452458c2a6630292629364221bff0dee@o4505709665976320'
+    + '.ingest.sentry.io/4505709666762752',
     integrations=[
         FlaskIntegration(),
     ],
-
     # Set traces_sample_rate to 1.0 to capture 100%
     # of transactions for performance monitoring.
     # We recommend adjusting this value in production.
     traces_sample_rate=1.0,
-
-    release='certificateservice:' + importlib.metadata.version("certificateservice"),
+    release='certificateservice:'
+    + importlib.metadata.version("certificateservice"),
     environment=os.environ.get('SENTRY_ENVIRONMENT', 'dev'),
 )
 
@@ -47,21 +53,25 @@ def urljoin_multipart(*args):
     """Join multiple parts of a URL together, ignoring empty parts."""
     logger.info('urljoin_multipart: %s', args)
     return '/'.join(
-        [arg.strip('/')
-         for arg in args if arg is not None and arg.strip('/') != '']
+        [
+            arg.strip('/')
+            for arg in args
+            if arg is not None and arg.strip('/') != ''
+        ]
     )
 
 
 try:
     from jupyterhub.services.auth import HubOAuth
+
     auth = HubOAuth(
-        api_token=os.environ['JUPYTERHUB_API_TOKEN'], cache_max_age=60)
+        api_token=os.environ['JUPYTERHUB_API_TOKEN'], cache_max_age=60
+    )
 except Exception:
     logger.warning('Auth system not configured')
     auth = None
 
-bp = Blueprint('certificateservice', __name__,
-               template_folder='templates')
+bp = Blueprint('certificateservice', __name__, template_folder='templates')
 
 url_prefix = os.getenv('JUPYTERHUB_SERVICE_PREFIX', '').rstrip('/')
 
@@ -73,17 +83,22 @@ def create_app():
     CORS(app)
 
     app.config['SECRET_KEY'] = os.environ.get(
-        'FLASK_SECRET', secrets.token_bytes(32))
+        'FLASK_SECRET', secrets.token_bytes(32)
+    )
     app.secret_key = app.config['SECRET_KEY']
 
-    app.config['CTACS_CERTIFICATE_DIR'] = \
-        os.environ.get('CTACS_CERTIFICATE_DIR', './certificate/')
-    app.config['CTACS_CABUNDLE'] = \
-        os.environ.get('CTACS_CABUNDLE', '/etc/cabundle.pem')
-    app.config['CTACS_CLIENTCERT'] = \
-        os.environ.get('CTACS_CLIENTCERT', '/tmp/x509up_u1000')
-    app.config['CTACS_DISABLE_ALL_AUTH'] = \
+    app.config['CTACS_CERTIFICATE_DIR'] = os.environ.get(
+        'CTACS_CERTIFICATE_DIR', './certificate/'
+    )
+    app.config['CTACS_CABUNDLE'] = os.environ.get(
+        'CTACS_CABUNDLE', '/etc/cabundle.pem'
+    )
+    app.config['CTACS_CLIENTCERT'] = os.environ.get(
+        'CTACS_CLIENTCERT', '/tmp/x509up_u1000'
+    )
+    app.config['CTACS_DISABLE_ALL_AUTH'] = (
         os.getenv('CTACS_DISABLE_ALL_AUTH', 'False') == 'True'
+    )
 
     # Check certificate folder
     os.makedirs(app.config['CTACS_CERTIFICATE_DIR'], exist_ok=True)
@@ -124,9 +139,12 @@ def download_authenticated(f):
             return f({'name': 'anonymous', 'admin': True}, *args, **kwargs)
         else:
             if auth is None:
-                return 'Unable to use jupyterhub to verify access to this\
+                return (
+                    'Unable to use jupyterhub to verify access to this\
                     service. At this time, the certificateservice uses jupyterhub\
-                    to control access to protected resources', 500
+                    to control access to protected resources',
+                    500,
+                )
 
             header = request.headers.get('Authorization')
             if header and header.startswith('Bearer '):
@@ -134,17 +152,23 @@ def download_authenticated(f):
             else:
                 header_token = None
 
-            service_token = session.get('service-token') \
-                or request.args.get('service-token') \
+            service_token = (
+                session.get('service-token')
+                or request.args.get('service-token')
                 or header_token
+            )
 
             if service_token:
                 service = auth.user_for_token(service_token)
                 if service is not None and not auth.check_scopes(
-                        'custom:certificateservice:download', service):
-                    return 'Access denied, token scopes are insufficient. ' + \
-                        'If you need access to this service, please ' + \
-                        'contact CTA-CH DC team at EPFL.', 403
+                    'custom:certificateservice:download', service
+                ):
+                    return (
+                        'Access denied, token scopes are insufficient. '
+                        + 'If you need access to this service, please '
+                        + 'contact CTA-CH DC team at EPFL.',
+                        403,
+                    )
             else:
                 user = None
 
@@ -154,10 +178,14 @@ def download_authenticated(f):
             if user_token:
                 user = auth.user_for_token(user_token)
                 if user is not None and not auth.check_scopes(
-                        'access:services!service=certificateservice', user):
-                    return 'Access denied, token scopes are insufficient. ' + \
-                        'If you need access to this service, please ' + \
-                        'contact CTA-CH DC team at EPFL.', 403
+                    'access:services!service=certificateservice', user
+                ):
+                    return (
+                        'Access denied, token scopes are insufficient. '
+                        + 'If you need access to this service, please '
+                        + 'contact CTA-CH DC team at EPFL.',
+                        403,
+                    )
             else:
                 user = None
 
@@ -180,9 +208,12 @@ def upload_authenticated(f):
             return f({'name': 'anonymous', 'admin': True}, *args, **kwargs)
         else:
             if auth is None:
-                return 'Unable to use jupyterhub to verify access to this\
+                return (
+                    'Unable to use jupyterhub to verify access to this\
                     service. At this time, the certificateservice uses jupyterhub\
-                    to control access to protected resources', 500
+                    to control access to protected resources',
+                    500,
+                )
 
             header = request.headers.get('Authorization')
             if header and header.startswith('Bearer '):
@@ -190,17 +221,23 @@ def upload_authenticated(f):
             else:
                 header_token = None
 
-            token = session.get('token') \
-                or request.args.get('token') \
+            token = (
+                session.get('token')
+                or request.args.get('token')
                 or header_token
+            )
 
             if token:
                 user = auth.user_for_token(token)
                 if user is not None and not auth.check_scopes(
-                        'access:services!service=certificateservice', user):
-                    return 'Access denied, token scopes are insufficient. ' + \
-                        'If you need access to this service, please ' + \
-                        'contact CTA-CH DC team at EPFL.', 403
+                    'access:services!service=certificateservice', user
+                ):
+                    return (
+                        'Access denied, token scopes are insufficient. '
+                        + 'If you need access to this service, please '
+                        + 'contact CTA-CH DC team at EPFL.',
+                        403,
+                    )
             else:
                 user = None
 
@@ -241,7 +278,8 @@ def get_certificate(user=None):
     if user is not None:
         filename = user_to_path_fragment(user) + ".crt"
         own_certificate_file = os.path.join(
-            app.config['CTACS_CERTIFICATE_DIR'], filename)
+            app.config['CTACS_CERTIFICATE_DIR'], filename
+        )
 
         if os.path.isfile(own_certificate_file):
             own_certificate = True
@@ -256,15 +294,14 @@ def get_certificate(user=None):
             certificate = f.read()
             if certificate_validity(certificate) <= datetime.now():
                 if own_certificate:
-                    raise 'Your configured certificate is invalid, ' + \
-                        'please refresh it.'
+                    raise 'Your configured certificate is invalid, ' + 'please refresh it.'
                 else:
                     logger.exception('outdated main certificate')
                     raise 'Service certificate invalid please contact us.'
-        
+
             return {
                 'certificate': certificate,
-                'cabundle': open(app.config['CTACS_CABUNDLE'], 'r').read()
+                'cabundle': open(app.config['CTACS_CABUNDLE'], 'r').read(),
             }, 200
     except FileNotFoundError:
         raise 'no valid certificate configured'
@@ -275,22 +312,29 @@ def get_certificate(user=None):
 def upload_certificate(user):
     filename = user_to_path_fragment(user) + ".crt"
     certificate_file = os.path.join(
-        app.config['CTACS_CERTIFICATE_DIR'], filename)
+        app.config['CTACS_CERTIFICATE_DIR'], filename
+    )
 
     certificate = request.json.get('certificate')
 
     try:
         cabundle = open(app.config['CTACS_CABUNDLE'], 'r').read()
     except FileNotFoundError:
-        return 'certificateservice cabundle not configured, ' + \
-            'please contact the administrator', 500
+        return (
+            'certificateservice cabundle not configured, '
+            + 'please contact the administrator',
+            500,
+        )
     verify_certificate(cabundle, certificate)
 
     validity = certificate_validity(certificate)
-    if validity.date() > date.today()+timedelta(days=7):
-        return 'certificate validity too long, please generate a ' +\
-            'short-lived (max 7 day) proxy certificate for uploading. ' +\
-            'Please see https://ctaodc.ch/ for more details.', 400
+    if validity.date() > date.today() + timedelta(days=7):
+        return (
+            'certificate validity too long, please generate a '
+            + 'short-lived (max 7 day) proxy certificate for uploading. '
+            + 'Please see https://ctaodc.ch/ for more details.',
+            400,
+        )
     if validity <= datetime.today():
         return 'certificate expired', 400
 
@@ -320,23 +364,32 @@ def upload_main_certificate(user):
         certificate = open(app.config['CTACS_CLIENTCERT'], 'r').read()
     verify_certificate(cabundle, certificate)
 
-    if certificate and certificate_validity(certificate).date() > \
-            (date.today()+timedelta(days=7)):
-        return 'certificate validity too long, please generate a ' +\
-            'short-lived (max 7 day) proxy certificate for uploading. ' +\
-            'Please see https://ctaodc.ch/ for more details.', 400
+    if certificate and certificate_validity(certificate).date() > (
+        date.today() + timedelta(days=7)
+    ):
+        return (
+            'certificate validity too long, please generate a '
+            + 'short-lived (max 7 day) proxy certificate for uploading. '
+            + 'Please see https://ctaodc.ch/ for more details.',
+            400,
+        )
 
     updated = set()
     if certificate is not None:
         with open(app.config['CTACS_CLIENTCERT'], 'w') as f:
             f.write(certificate)
             updated.add('Certificate')
-        os.chmod(app.config['CTACS_CLIENTCERT'], stat.S_IWUSR) # Write by owner
+        os.chmod(
+            app.config['CTACS_CLIENTCERT'], stat.S_IWUSR
+        )  # Write by owner
     if cabundle is not None:
         with open(app.config['CTACS_CABUNDLE'], 'w') as f:
             f.write(cabundle)
             updated.add('CABundle')
-        os.chmod(app.config['CTACS_CABUNDLE'], stat.S_IWUSR | stat.S_IRGRP | stat.S_IROTH) 
+        os.chmod(
+            app.config['CTACS_CABUNDLE'],
+            stat.S_IWUSR | stat.S_IRGRP | stat.S_IROTH,
+        )
 
     return {
         'message': ' and '.join(updated) + ' stored',
@@ -350,11 +403,11 @@ def health():
     # TODO: health status check
     return 'OK', 200
 
-    if r.status_code in [200, 207]:
-        return 'OK', 200
-    else:
-        logger.error('service is unhealthy: %s', r.content.decode())
-        return 'Unhealthy!', 500
+    # if r.status_code in [200, 207]:
+    #     return 'OK', 200
+    # else:
+    #     logger.error('service is unhealthy: %s', r.content.decode())
+    #     return 'Unhealthy!', 500
 
 
 @app.route(url_prefix + '/oauth_callback')
